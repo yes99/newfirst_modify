1e56e2c82a3ac3a1d56f0d6f99dda222a33afd5f
yes99 <yes950324@naver.com>
2021-09-27 17:25:59 +0900

    회사에서 구현 성공

--- src/main/java/com/example/newfirst/board/mapper/BoardMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.newfirst.board.mapper;
-
-import org.springframework.stereotype.Repository;
-
-
-@Repository("com.example.newfirst.board.mapper.BoardMapper")
-public interface BoardMapper {
-    public int boardCount() throws Exception;
-}
